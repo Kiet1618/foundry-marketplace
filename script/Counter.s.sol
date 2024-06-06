b20d5169baf81b745995e0fb6db4684cc9cf0da8
Kiet1618 <ngokiet3207@gmail.com>
2024-06-06 08:54:09 +0700

    test

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,12 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-import {Script, console} from "forge-std/Script.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract GameItem is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    function run() public {
-        vm.broadcast();
+    constructor() ERC721("GameItem", "ITM") {}
+
+    function awardItem(
+        address player,
+        string memory tokenURI
+    ) public returns (uint256) {
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
     }
 }
