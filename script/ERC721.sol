2e81fb04712a7dbdb0c32297209537618a614c76
Kiet1618 <ngokiet3207@gmail.com>
2024-06-07 09:57:58 +0700

    chore: Update foundry.toml and remappings.txt

--- script/ERC721.sol
+++ script/ERC721.sol
@@ -3,10 +3,26 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 
-contract ERC721 is Script {
+contract L3Exchange is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+
+        // address l3Exchange = Upgrades.deployProxy(
+        //     address(l3Exchange),
+        //     address(
+        //         Options({
+        //             value: 0,
+        //             salt: 0,
+        //             nonce: 0,
+        //             kind: 0,
+        //             admin: address(0),
+        //             beacon: address(0),
+        //             implementation: address(0),
+        //             initializer: abi.encodeWithSignature("initialize()")
+        //         })
+        //     )
+        // );
     }
 }
--- test/L3ExchangeUpgradeable.t.sol
+++ test/L3ExchangeUpgradeable.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
@@ -8,14 +8,19 @@ import {L3ExchangeUpgradeable} from "../src/L3ExchangeUpgradeable.sol";
 import {ERC721Sample} from "../src/ERC721Sample.sol";
 import {WETH9} from "../src/WETH.sol";
 
+import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import {IBeacon} from "@openzeppelin/contracts/proxy/beacon/IBeacon.sol";
+import {OrderStructs} from "../src/libraries/OrderStructs.sol";
+
+import {QuoteType} from "../src/enums/QuoteType.sol";
+
+import {CollectionType} from "../src/enums/CollectionType.sol";
+
 contract L3ExchangeUpgradeableTest is Test, IERC721Receiver {
     L3ExchangeUpgradeable l3Exchange;
-    L3ExchangeUpgradeable l3ExchangeProxy;
     ERC721Sample erc721Sample;
     WETH9 weth;
-
-    address _admin = makeAddr("ADMIN");
-    address _operator = makeAddr("OPERATOR");
     address _addr1 = makeAddr("ADDR1");
     address _addr2 = makeAddr("ADDR2");
 
@@ -28,26 +33,90 @@ contract L3ExchangeUpgradeableTest is Test, IERC721Receiver {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function setUp() public {
-        l3Exchange = new L3ExchangeUpgradeable();
-        erc721Sample = new ERC721Sample();
+    function testUUPS() public {
         weth = new WETH9();
 
-        l3ExchangeProxy = L3ExchangeUpgradeable.initialize(
-            "L3Exchange",
-            "1",
-            _admin,
-            _operator,
-            address(erc721Sample),
-            address(weth),
-            address(weth)
+        address proxyERC721 = Upgrades.deployUUPSProxy(
+            "ERC721Sample.sol",
+            abi.encodeCall(
+                ERC721Sample.initialize,
+                (address(this), "ERC721Sample", "COL")
+            )
         );
+
+        erc721Sample = ERC721Sample(payable(proxyERC721));
+
+        address proxy = Upgrades.deployUUPSProxy(
+            "L3ExchangeUpgradeable.sol",
+            abi.encodeCall(
+                L3ExchangeUpgradeable.initialize,
+                (
+                    "L3ExchangeUpgradeable",
+                    "1.0.0",
+                    address(this),
+                    address(this),
+                    address(erc721Sample),
+                    address(erc721Sample),
+                    address(erc721Sample)
+                )
+            )
+        );
+
+        l3Exchange = L3ExchangeUpgradeable(payable(proxy));
     }
 
-    function test_Initialize() public view {
-        assertEq(
-            l3Exchange.hasRole(l3Exchange.DEFAULT_ADMIN_ROLE(), address(this)),
-            true
-        );
-    }
+    // function testExecuteOrder() public {
+    //     erc721Sample.mint(address(this));
+
+    //     erc721Sample.setApprovalForAll(address(l3Exchange), true);
+
+    //     OrderStructs.Maker memory maker = OrderStructs.Maker({
+    //         quoteType: QuoteType.Ask,
+    //         orderNonce: 0,
+    //         collectionType: CollectionType.ERC721,
+    //         collection: address(erc721Sample),
+    //         tokenId: 1,
+    //         currency: address(weth),
+    //         price: 10,
+    //         signer: address(this),
+    //         startTime: 1717495754,
+    //         endTime: 1817495738,
+    //         assets: new address[](0),
+    //         values: new uint256[](0),
+    //         makerSignature: new bytes(0)
+    //     });
+    //     bytes32 _MAKER_TYPEHASH = 0x5f3e890c36d263fd3e4b97d606b6456effba4409d05897000409303ba8dcf2f4;
+
+    //     bytes32 signature = keccak256(
+    //         bytes.concat(
+    //             abi.encode(
+    //                 _MAKER_TYPEHASH,
+    //                 maker.quoteType,
+    //                 maker.orderNonce,
+    //                 maker.collectionType,
+    //                 maker.collection,
+    //                 maker.tokenId,
+    //                 maker.currency,
+    //                 maker.price,
+    //                 maker.signer,
+    //                 maker.startTime,
+    //                 maker.endTime,
+    //                 keccak256(abi.encodePacked(maker.assets)),
+    //                 keccak256(abi.encodePacked(maker.values))
+    //             )
+    //         )
+    //     );
+
+    //     maker.makerSignature = abi.encodePacked(signature);
+
+    //     l3Exchange.executeOrder(
+    //         maker,
+    //         OrderStructs.Taker({
+    //             recipient: _addr1,
+    //             takerSignature: new bytes(0)
+    //         })
+    //     );
+
+    //     assertEq(weth.balanceOf(address(this)), 1);
+    // }
 }
