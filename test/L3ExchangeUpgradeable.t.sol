f34bb1c133304c340c4f20278a362d13db61e9dd
Kiet1618 <ngokiet3207@gmail.com>
2024-06-07 09:58:12 +0700

    chore: Update Solidity version to 0.8.2

--- test/L3ExchangeUpgradeable.t.sol
+++ test/L3ExchangeUpgradeable.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.2;
 
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
